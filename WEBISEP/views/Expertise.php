58d910afa072e64021934377ab48530223456e4d
LucasMichaud <lucasmichaud1@gmail.com>
2019-05-15 10:55:13 +0200

    Update Expertise.php

--- WEBISEP/views/Expertise.php
+++ WEBISEP/views/Expertise.php
@@ -3,20 +3,20 @@ include("General.php")
    ?>
 <html>
 <head>
-	<title>Expertise</title>
-	<link rel="stylesheet" href="design/Expertise.css" />
+  <title>Expertise</title>
+  <link rel="stylesheet" href="design/Expertise.css" />
 
-		<meta charset="utf-8">
+    <meta charset="utf-8">
 </head>
 <body>
 
-	<div class="fond">	
-			<h1>Expertise</h1>
+  <div class="fond">  
+      <h1>Expertise</h1>
 
-		<p class="description">Sécurisep est un projet qui vise à démocratiser la maison connecter dans le monde. Nous voulons faciliter la vie de nos clients grâce à nos fonctionnalitées présentees sur notre site. Ce projet à commencer en septembre 2018 et à vu le jour grâce à Domisep, une enreprise </p>
+    <p class="description">Sécurisep est un projet qui vise à démocratiser la maison connecter dans le monde. Nous voulons faciliter la vie de nos clients grâce à nos fonctionnalitées présentees sur notre site. Ce projet à commencer en septembre 2018 et à vu le jour grâce à Domisep, une enreprise </p>
 </div>
-	<h2>Catalogue</h2>
-	
+  <h2>Catalogue</h2>
+  
 <div class="catalogue">
 
 <?php   $compteur=0;
@@ -29,18 +29,19 @@ include("General.php")
 
         <h3> 
            <?php
-           echo $catalogue['nom'];?>
+           echo $catalogue['CatName'];?>
         </h3>
 
         <p class="float">
-        <img class="image" src="image/<?php echo $catalogue['image'] ?>" alt="capteur" id="capteur"/></p>
+        <img class="image" src="image/<?php echo $catalogue['CatImage'] ?>" alt="capteur" id="capteur"/></p>
 
         <p class="text">
         <span>Description</span> :<br>   
            <?php catalogue($compteur);
-           	echo $catalogue['description']; ?>
-        <?php echo 'Prix : ' . $catalogue['prix'].' €' ?>
-        <?php echo 'Poids : ' .$catalogue['poids'].' g' ?>
+            echo $catalogue['CatDesc']; ?>
+        <?php echo 'Prix : ' . $catalogue['CatPrice'].' €' ?>
+        <?php echo 'Poids : ' .$catalogue['CatWeight'].' g' ?>
+        <?php echo 'Modèle : ' .$catalogue['CatType'] ?>
         </p>
 
     </div>
